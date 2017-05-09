d9f2fde7b44443ecce1db6a38e418867d3f3804c
Abdul Wahab <cs.abdulwahab@gmail.com>
2017-05-10 01:19:31 +0500

    shared preference

---
+++ app/src/main/java/com/example/abdul_wahab/madsp17sharedpreference/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.abdul_wahab.madsp17sharedpreference;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        SharedPreferences sp = getSharedPreferences("abc", MODE_PRIVATE);
+
+        if (sp != null && (sp.getInt("k1",-999)!=999) ) {
+
+            SharedPreferences.Editor et = sp.edit();
+            et.putInt("k1", 123);
+            et.apply();
+            //s
+
+        }
+
+
+
+
+
+
+
+
+    }
+}
